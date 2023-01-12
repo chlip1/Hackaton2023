6d8628b64adf51dd676b4fd94d4a6811bdfab930
xXMaKuSXx <93930187+xXMaKuSXx@users.noreply.github.com>
2023-01-12 18:37:14 +0100

    duzy problem ale to nic

--- map/views.py
+++ map/views.py
@@ -11,7 +11,11 @@ import pandas as pd
 locations = []
 
 def index(request):
+
+    return render(request, 'glowna.html')
+# def index(request):
     
+<<<<<<< Updated upstream
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
@@ -35,6 +39,73 @@ def index(request):
         return HttpResponse('You address input is invalid')
 
     
+=======
+#     if request.method == 'POST':
+#         form = SearchForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('/')
+#     else:
+#         form = SearchForm()
+#     address = Search.objects.all().last()
+#     location = geocoder.osm(address)
+#     lat = location.lat
+#     lng = location.lng
+#     country = location.country
+#     data = pd.read_csv("C:\\Users\\jakub\\OneDrive\\Pulpit\\stronka\\Hackaton2023\\average-latitude-longitude-countries.csv")
+#     latitude = data['Latitude'].tolist()
+#     longitude = data['Longitude'].tolist()
+#     print(latitude)
+#     locations.append([lat,lng])
+#     print(locations)
+#     if lat == None or lng == None:
+#         address.delete()
+#         return HttpResponse('You address input is invalid')
+
+
+    # Create Map Object
+    # m = folium.Map(location=[55, 18], zoom_start=10)
+    #
+    # for x in range(0, len(longitude)):
+    #     folium.Marker((latitude[x],longitude[x]), icon=folium.DivIcon(html=f"""
+    #         <div><svg>
+    #             <circle r="10" fill="#69b3a2"/>
+    #         </svg></div>""")).add_to(m)
+    #
+    # folium.Marker([lat, lng], tooltip='Click for more',
+    #               popup=country).add_to(m)
+    # Get HTML Representation of Map Object
+    # m = m._repr_html_()
+#     context = {
+#         'm': m,
+#         'form': form,
+#     }
+#     return render(request, 'index.html', context)
+# locations = []
+def search(request):
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = SearchForm()
+    address = Search.objects.all().last()
+    location = geocoder.osm(address)
+    lat = location.lat
+    lng = location.lng
+    country = location.country
+    data = pd.read_csv("C:\\Users\\jakub\\OneDrive\\Pulpit\\stronka\\Hackaton2023\\average-latitude-longitude-countries.csv")
+    latitude = data['Latitude'].tolist()
+    longitude = data['Longitude'].tolist()
+    print(latitude)
+    locations.append([lat, lng])
+    print(locations)
+    if lat == None or lng == None:
+        address.delete()
+        return HttpResponse('You address input is invalid')
+
+>>>>>>> Stashed changes
     # Create Map Object
     m = folium.Map(location=[19, -12], zoom_start=2)
 
