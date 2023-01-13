5a139095f3282642330c015debdab6723779e16b
xXMaKuSXx <93930187+xXMaKuSXx@users.noreply.github.com>
2023-01-13 07:35:52 +0100

    Update prepareFiles.py

--- prepareFiles.py
+++ prepareFiles.py
@@ -10,6 +10,17 @@ def deleteTooFar(data, lat, lon):
         i.data = i.data.sort_values(by=['abs'])
         i.data = i.data[0:1]
         df = pd.merge(df, i.data, how='outer')
-        df['near'] = np.where(df['abs'] < 2, True, False)
+        df['near'] = np.where(df['abs'] < 1.8, True, False)
     return df
- 
+
+def deleteTooFar_2(data, lat, lon):
+    w_lat = 111
+    w_lon = 64.8
+    df = pd.DataFrame(columns=['lat', 'lon', 'type', 'title', 'abs'])
+    for i in data:
+        i.data['abs']= np.sqrt((((i.data['lon'] - lon) * w_lon) ** 2 ) + ((i.data['lat'] - lat) * w_lat) ** 2)
+        i.data = i.data.sort_values(by=['abs'])
+        i.data = i.data[0:1]
+        df = pd.merge(df, i.data, how='outer')
+        df['near'] = np.where(df['abs'] < 3.6, True, False)
+    return df
