94670e960f50d85fd88673eb174b453a84866086
xXMaKuSXx <93930187+xXMaKuSXx@users.noreply.github.com>
2023-01-13 03:36:02 +0100

    Update prepareFiles.py

--- prepareFiles.py
+++ prepareFiles.py
@@ -1,8 +1,15 @@
 import pandas as pd
-
-def deleteTooFar(data, loc, lng):
+import numpy as np
+ 
+def deleteTooFar(data, lat, lon):
+    w_lat = 111
+    w_lon = 64.8
+    df = pd.DataFrame(columns=['lat', 'lon', 'type', 'title', 'abs'])
     for i in data:
-        print(loc)
-        print(i.data)
-        i.data = i.data.sort_values(by=['lat'], key=lambda x: abs(i.data['lat']-loc))
-        print(i.data)
+        i.data['abs']= np.sqrt((((i.data['lon'] - lon) * w_lon) ** 2 ) + ((i.data['lat'] - lat) * w_lat) ** 2)
+        i.data = i.data.sort_values(by=['abs'])
+        i.data = i.data[0:1]
+        df = pd.merge(df, i.data, how='outer')
+        df['near'] = np.where(df['abs'] < 2, True, False)
+    return df
+ 
